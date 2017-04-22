f56088e5479984213b89c15986ca2974ea69c991
Александр Шинкевич <a.shinkevich1998@gmail.com>
2017-04-22 14:11:22 +0300

    Добавлена функция тестирования

--- main.cpp
+++ main.cpp
@@ -8,7 +8,23 @@ struct Temperature {
 istream& operator>>(istream& stream, Temperature& temperature) {
     stream >> temperature.value >> temperature.scale;
 }
+void test_temperature_input() {
+    Temperature temperature;
+    istringstream heh("10K");
+    heh >> temperature;
+    assert (temperature.value == 10);
+    assert (temperature.scale == 'K');
+    istringstream kek("10F");
+    kek >> temperature;
+    assert (temperature.value == 10);
+    assert (temperature.scale == 'F');
+    istringstream lol("10C");
+    lol >> temperature;
+    assert (temperature.value == 10);
+    assert (temperature.scale == 'C');
+}
 int main() {
+    Temperature temperature;
     size_t number_count;
     cerr << "Enter number count: ";
     cin >> number_count;
